6b094198bd94b99c121263800a02065aefbf4195
CrazyMeowl <nooctran9904@gmai.com>
2021-04-28 08:51:35 +0700

    update the sql backup file

--- 28-04-2021.sql
+++ 28-04-2021.sql
@@ -16,6 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `location`
+--
+
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `location` (
+  `locationID` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `timezone` int DEFAULT NULL,
+  `region` varchar(50) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `longitude` float DEFAULT NULL,
+  `latitude` float DEFAULT NULL,
+  `elevation` float DEFAULT NULL,
+  PRIMARY KEY (`locationID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `location`
+--
+
+LOCK TABLES `location` WRITE;
+/*!40000 ALTER TABLE `location` DISABLE KEYS */;
+INSERT INTO `location` VALUES (623,'Paris',2,'Europe','France',2.351,48.857,35),(22889,'Sydney',10,'Oceania','Australia',151.209,-33.87,1),(178087,'Berlin',2,'Europe','Germany',13.406,52.518,35),(208971,'Jakarta',7,'Asia','Indonesia',-6.175,106.827,11),(230276,'Dalat',8,'Asia','Malaysia',111.939,2.743,4),(300597,'Singapore',8,'Asia','Singapore',103.843,1.311,13),(327659,'Washington',-4,'North America','United States',-77.036,38.895,42),(328328,'London',1,'Europe','United Kingdom',-0.107,51.514,18),(347625,'Los Angeles',-7,'North America','United States',-118.244,34.052,121),(349727,'New York',-4,'North America','United States',-73.969,40.779,8),(351194,'Dallas',-5,'North America','United States',-96.807,32.783,129),(351820,'Long Xuyen',7,'Asia','Vietnam',105.444,10.374,1),(352499,'Ca Mau',7,'Asia','Vietnam',105.149,9.178,1),(353412,'Hanoi',7,'Asia','Vietnam',105.7,20.999,19),(353981,'Ho Chi Minh City',7,'Asia','Vietnam',106.702,10.775,7),(356204,'Hue',7,'Asia','Vietnam',107.599,16.453,6),(414495,'Thu Duc',7,'Asia','Vietnam',106.78,10.87,17),(979978,'Hehe Township',8,'Asia','China',113.756,35.332,78);
+/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `wfd`
 --
 
@@ -55,4 +85,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-28  8:33:39
+-- Dump completed on 2021-04-28  8:50:38
